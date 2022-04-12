611a939c107616fd6c4b848d6412cec2d69ae251
muhadiyaksa <muhadiyaksa@gmail.com>
2022-04-12 09:48:03 +0700

    Create CountDown Layout

--- main.js
+++ main.js
@@ -60,3 +60,41 @@ function indicatorActive(params) {
 
   indicatorAll[params].classList.add("active");
 }
+
+// Set the date we're counting down to
+let countDownDate = new Date("May 15, 2022 23:30:59").getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function () {
+  // Get today's date and time
+  let now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("day-one").innerHTML = days.toString().split("")[0];
+  document.getElementById("day-two").innerHTML = days.toString().split("")[1];
+  document.getElementById("hour-one").innerHTML = hours.toString().split("")[0];
+  document.getElementById("hour-two").innerHTML = hours.toString().split("")[1];
+  document.getElementById("minute-one").innerHTML = minutes.toString().split("")[0];
+  document.getElementById("minute-two").innerHTML = minutes.toString().split("")[1];
+
+  document.getElementById("day-one-waktu").innerHTML = days.toString().split("")[0];
+  document.getElementById("day-two-waktu").innerHTML = days.toString().split("")[1];
+  document.getElementById("hour-one-waktu").innerHTML = hours.toString().split("")[0];
+  document.getElementById("hour-two-waktu").innerHTML = hours.toString().split("")[1];
+  document.getElementById("minute-one-waktu").innerHTML = minutes.toString().split("")[0];
+  document.getElementById("minute-two-waktu").innerHTML = minutes.toString().split("")[1];
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
